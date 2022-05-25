ef573ae0f419b81cf08d2200086228bb6d1b5175
Kayomn <kayomn@kayomn.net>
2022-05-25 03:38:19 +0000

    Tidy up documentation comments in scripts

--- scripts/github.js
+++ scripts/github.js
@@ -2,8 +2,10 @@
 const url = "https://api.github.com/"
 
 /**
+ * Fetches a manifest of all public Github repositories belonging to a given user.
+ *
  * @param {string} username Identifier of the target Github user.
- * @returns A @see promise of an array containing all public repository metadata for a given user.
+ * @returns @see Promise of an array containing all public repository metadata for a given user.
  */
-export const fetchRepos = username => fetch(`${url}/users/${username}/repos`)
+export const fetchRepos = username => fetch(`${url}users/${username}/repos`)
 										.then(response => response.json())
